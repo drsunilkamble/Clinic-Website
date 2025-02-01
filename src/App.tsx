41e1678548c016f016cd60cfeb303aa01206d655
jadhavharshh <harshjadhavconnect@gmail.com>
2025-02-01 16:02:30 +0530

    Add SmoothScrollbar component and integrate smooth scrolling in App

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import ServicesSection from './Sections/ServiceSection'
 import ReviewSection from './Sections/ReviewSection'
 import FooterSection from './Sections/FooterSection'
 import AboutDoctor from './Sections/AboutDoctor'
+import SmoothScrollWrapper from './components/ui/SmoothScrollbar'
 
 function App() {
 
   return (
     <>
-      <div className="w-full bg-gradient-to-br from-slate-50 to-blue-50">
-      <div className="relative min-h-screen flex flex-col items-center justify-start overflow-hidden">
-        <NavigationBar />
-        <HeroSection />
-        <VisionSection />
-        <AboutDoctor />
-        <ServicesSection />
-        <ReviewSection />
-        <FooterSection />
-      </div>
-      </div>
+      <SmoothScrollWrapper>
+        <div className="w-full bg-gradient-to-br from-slate-50 to-blue-50">
+          <div className="relative min-h-screen flex flex-col items-center justify-start overflow-hidden">
+            <NavigationBar />
+            <HeroSection />
+            <VisionSection />
+            <AboutDoctor />
+            <ServicesSection />
+            <ReviewSection />
+            <FooterSection />
+          </div>
+        </div>
+      </SmoothScrollWrapper>
     </>
   )
 }
